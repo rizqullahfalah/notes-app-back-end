e825daa12d13f87a1e298d760190c29636128d75
Rizqullah Falah Mahendra <rizqullahmahendra@gmail.com>
2025-05-31 21:18:52 +0700

    fix bugs host value

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
- const server = Hapi.server({
-  port: 5000,
-  host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-  routes: {
-    cors: {
-      origin: ['*'],
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],  // agar bisa diakses dari mana saja
+      },
     },
-  },
-});
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
